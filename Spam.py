e1f0794272673568383435c77289582d066cc814
yavda1 <94653933+yavda1@users.noreply.github.com>
2021-11-18 16:23:36 -0800

    Update Spam.py

--- Spam.py
+++ Spam.py
@@ -1,13 +1,6 @@
 import webbrowser
-#import time
 
 spam = 0
-#pcCrash = 5
-#while spam < pcCrash :
-	#webbrowser.open("https://www.youtube.com/channel/UCnV9orb2fg2TXYcHJsNVDQQ")
-	#pcCrash += 1
-#spam = False
-
 
 while spam < 20:
     webbrowser.open("https://www.youtube.com/channel/UCnV9orb2fg2TXYcHJsNVDQQ")
