77d579f2fc678f340d577ae55fc15d25e690e45d
yavda1 <94653933+yavda1@users.noreply.github.com>
2021-11-21 13:05:48 -0800

    Update and rename Spam.py to main.py

--- Spam.py
+++
@@ -1,7 +0,0 @@
-import webbrowser
-
-spam = 0
-
-while spam < 20:
-    webbrowser.open("https://www.youtube.com/channel/UCnV9orb2fg2TXYcHJsNVDQQ")
-    spam += 1
---
+++ main.py
@@ -0,0 +1,7 @@
+import webbrowser #imports
+
+spam = 0 #Leave this unchanged
+
+while spam < 20: #Change the 20 to however many tabs you want it to open
+    webbrowser.open("https://www.youtube.com/channel/UCnV9orb2fg2TXYcHJsNVDQQ") #Change the link to whatever you want, I have made it my youtube channel
+    spam += 1 #Leave this unchanged
