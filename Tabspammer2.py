db624db2a0516c56fda554b5f0450cf0e61f97d6
yavda1 <94653933+yavda1@users.noreply.github.com>
2021-12-21 14:00:18 -0800

    Update Tabspammer2.py

--- Tabspammer2.py
+++ Tabspammer2.py
@@ -1,22 +1,23 @@
+#Imports
 import random
 import webbrowser
 
-Run = 0
-W1 = "https://www.youtube.com/channel/UCnV9orb2fg2TXYcHJsNVDQQ"
+Run = 0 #Leave this
+W1 = "https://docs.microsoft.com/en-us/dotnet/csharp/" #Change these to any website you want
 W2 = "https://www.python.org"
 W3 = "https://www.oracle.com/java/technologies/downloads/"
 W4 = "https://www.javascript.com/"
-while Run < 20:
-	Number = random.randint(1, 4)
-	if Number == 1:
-		Run += 1
-		webbrowser.open(W1)
+while Run < 20: #Change the 20 to however many tabs you want to open
+	Number = random.randint(1, 4) #Choosing a random number
+	if Number == 1: #If the "Number" variable is 1 this happens
+		Run += 1 #Leave this
+		webbrowser.open(W1) #Opening the W1 variable
 	elif Number == 2:
 		Run += 1
-		webbrowser.open(W2)
+		webbrowser.open(W2) #Opening the W2 variable
 	elif Number == 3:
 		Run += 1
-		webbrowser.open(W3)
+		webbrowser.open(W3) #Opening the W3 variable
 	elif Number == 4:
-		webbrowser.open(W4)
+		webbrowser.open(W4) #Opening the W4 variable
 		Run += 1
