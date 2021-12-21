e8cb94a30f8442eb5e295d86e720d324403d8810
yavda1 <94653933+yavda1@users.noreply.github.com>
2021-12-21 13:46:10 -0800

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import webbrowser #imports
 spam = 0 #Leave this unchanged
 
 while spam < 20: #Change the 20 to however many tabs you want it to open
-    webbrowser.open("https://www.youtube.com/channel/UCnV9orb2fg2TXYcHJsNVDQQ") #Change the link to whatever you want, I have made it my youtube channel
+    webbrowser.open("python.org") #Change the link to whatever you want, I have made it 
     spam += 1 #Leave this unchanged
