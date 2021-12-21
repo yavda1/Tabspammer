2a0f5e1c486c4cf913b660899ca27b5ae0e09cf1
yavda1 <94653933+yavda1@users.noreply.github.com>
2021-12-21 13:46:32 -0800

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import webbrowser #imports
 spam = 0 #Leave this unchanged
 
 while spam < 20: #Change the 20 to however many tabs you want it to open
-    webbrowser.open("python.org") #Change the link to whatever you want, I have made it 
+    webbrowser.open("python.org") #Change the link to whatever you want, I have made it python.org
     spam += 1 #Leave this unchanged
